2c0cb7ac347f858feb357e151112824f3b1a3b76
Keerthi Surakanti <157736147+ksurakan@users.noreply.github.com>
2024-02-22 20:09:14 -0600

    ex4

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -16,12 +16,13 @@ FILENAME = get_data_file_path("messages.log")
 
 
 def time_between_shutdowns(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-    pass
+    time_between_shutdowns(logfile):
+    shutdown_events = get_shutdown_events(logfile)
+    start_date = logstamp_to_datetime(shutdown_events[0][5:24])
+    end_date = logstamp_to_datetime(shutdown_events[-1][5:24])
+    return end_date-start_date
 
-
+    
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{time_between_shutdowns(FILENAME)=}')
